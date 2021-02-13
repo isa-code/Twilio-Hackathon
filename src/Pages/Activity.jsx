73839f673717d8df6f7912e668bb014c590dd784
isabelle Guilherme <isabelle.guilherme@csenen.com>
2021-02-13 23:33:39 +0100

    modale css et test google font

--- src/Pages/Activity.jsx
+++ src/Pages/Activity.jsx
@@ -3,36 +3,76 @@ import ActivitySubscribe from "../Components/ActivitySubscribe";
 import ActivityContent from "../Components/ActivityContent";
 import ActivitySubsActivModal from "../Components/ActivitySubsActivModal";
 import CreateActivity from "../Pages/CreateActivity";
+import ReactModal from "react-modal";
+import "../Components/Modal.css";
 
 class Activity extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state={
-            modalActiv: false
-        }
-    }
-    
-    displayModalActiv = (event) => {
-        this.setState({modalActiv:true})
-    }
-    
-    render(){
-        return(
-            <div>
-                <ActivityContent />
-                <ActivitySubscribe 
-                    activOnEvent={this.displayModalActiv} 
-                />
+    this.state = {
+      modalActiv: false,
+      modalNew: false,
+      IsOpen: true,
+    };
+  }
 
-                <ActivitySubsActivModal 
-                    info={this.state.modalActiv} />
-                }
+  displayModalActiv = (event) => {
+    this.setState({ modalActiv: true });
+  };
 
-                <CreateActivity />
+  displayModalNew = (event) => {
+    this.setState({ modalNew: true });
+  };
+
+  handleClose = () => {
+    this.setState({ IsOpen: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <ActivityContent />
+
+        <ActivitySubscribe
+          activOnEvent={this.displayModalActiv}
+          newOnEvent={this.displayModalNew}
+        />
+
+        {this.state.modalActiv == true && (
+          <ReactModal isOpen={true}>
+            <div className="modal">
+              <p>Done :-)</p>
             </div>
-        )
-    }
+            <button
+              type="button"
+              className="modalBtnClose"
+              onClick={this.handleClose}
+            >
+              Fermer
+            </button>
+          </ReactModal>
+        )}
+
+        {this.state.modalNew == true && (
+          <ReactModal isOpen={true}>
+            <div className="modal">
+              <p>Done :-)</p>
+            </div>
+            <button
+              type="button"
+              className="modalBtnClose"
+              onClick={this.handleClose}
+            >
+              Fermer
+            </button>
+          </ReactModal>
+        )}
+
+        <CreateActivity />
+      </div>
+    );
+  }
 }
 
 export default Activity;
--- src/Pages/CreateActivity.jsx
+++ src/Pages/CreateActivity.jsx
@@ -1,45 +1,35 @@
-import Reat from "react";
 import { Component } from "react";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
 class CreateActivity extends Component {
-    constructor(props){
-        super(props)
-
-        this.state={
-            date:""
-        }
-    }
-
-    changeDate = (event) => {
-        alert(teeeest)
-    }
-    
-    render(){
-        return(
-            <div>
-                <select id="CreateActivSelect">
-                    <option value="">Chose a mission</option>
-                    <option value="">Street maraud</option>
-                    <option value="">Guidance</option>
-                    <option value="">Social guidance</option>
-                    <option value="">A night in a warm bed</option>
-                    <option value="">Social guidance</option>
-                    <option value="">Pampering</option>
-                </select>
-
-                <input type="text" placeholder="Chose a title for your mission" />
-
-                <DatePicker 
-                    onChange={this.changeDate(event)}
-                    selected={this.state.date} />
-
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "",
+    };
+  }
+  changeDate = (event) => {
+    // alert(teeeest);
+  };
+  render() {
+    return (
+      <div>
+        <select id="CreateActivSelect">
+          <option value="">Chose a mission</option>
+          <option value="">Street maraud</option>
+          <option value="">Guidance</option>
+          <option value="">Social guidance</option>
+          <option value="">A night in a warm bed</option>
+          <option value="">Social guidance</option>
+          <option value="">Pampering</option>
+        </select>
+        <input type="text" placeholder="Chose a title for your mission" />
+        <DatePicker onChange={this.changeDate()} selected={this.state.date} />
+      </div>
+    );
+  }
 }
 
 export default CreateActivity;
